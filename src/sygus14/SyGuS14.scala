ba84055bfcac61c2f984071e030e021394694d3c
Iwo Bladek <iwo.bladek@cs.put.poznan.pl>
2017-10-23 21:29:01 +0200

    Minor

--- src/sygus14/SyGuS14.scala
+++ src/sygus14/SyGuS14.scala
@@ -35,7 +35,7 @@ object SyGuS14 {
       "BitVec", "Array", "Int", "Bool", "Enum", "Real", "Constant", "Variable", "InputVariable",
       "LocalVariable", "let", "true", "false", "forall", "exists")
 
-    def guardedSymbol: Parser[String] = "|" ~> ".*".r <~ "|" ^^ { s => s"|$s|" }
+    def guardedSymbol: Parser[String] = "|.*|".r ^^ { s => s }
 
     def symbol = (guardedSymbol | """[a-zA-Z\-[_\+\*&\|\!~<>=/%\?\.\$\^]]([a-zA-Z0-9\-[_\+\*&\|\!~<>=/%\?\.\$\^]])*""".r) ^^ {
       case s => if (reservedWords.contains(s)) 
