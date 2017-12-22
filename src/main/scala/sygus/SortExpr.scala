b1d89f92419cef09e19af3fd6e897b16757faf2d
Iwo Błądek <iwo.bladek@cs.put.poznan.pl>
2017-12-22 17:02:51 +0100

    Add name to SortExpr

--- src/main/scala/sygus/SortExpr.scala
+++ src/main/scala/sygus/SortExpr.scala
@@ -1,18 +1,25 @@
 package sygus
 
-sealed trait SortExpr
-case class IntSortExpr() extends SortExpr
-case class BoolSortExpr() extends SortExpr
-case class RealSortExpr() extends SortExpr
+sealed trait SortExpr {
+  def name: String
+}
+case class IntSortExpr() extends SortExpr { val name = "Int" }
+case class BoolSortExpr() extends SortExpr { val name = "Bool" }
+case class RealSortExpr() extends SortExpr { val name = "Real" }
 case class BitVecSortExpr(n: Int) extends SortExpr {
   require(n > 0)
+  val name = s"BitVec[$n]"
 }
 
-case class EnumSortExpr(symbols: List[String]) extends SortExpr
-case class ArraySortExpr(sortExpr1: SortExpr, sortExpr2: SortExpr) extends SortExpr
-case class SymbolSortExpr(symbol: String) extends SortExpr
+case class EnumSortExpr(symbols: List[String]) extends SortExpr {
+  val name = s"Enum(${symbols.mkString(",")})"
+}
+case class ArraySortExpr(sortExpr1: SortExpr, sortExpr2: SortExpr) extends SortExpr {
+  val name = s"Array[$sortExpr1,$sortExpr2]"
+}
+case class SymbolSortExpr(symbol: String) extends SortExpr { val name = symbol }
 
-// SuGuS16:
-case class StringSortExpr() extends SortExpr
+// SyGuS16:
+case class StringSortExpr() extends SortExpr { val name = "String" }
 
   // End /////////////////////////////////////////////////////////////
