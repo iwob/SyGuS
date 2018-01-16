2be6947219cbce6b40b368159f8923da3bf3dc4c
Iwo Bladek <iwo.bladek@cs.put.poznan.pl>
2018-01-16 16:19:11 +0100

    Minor

--- src/main/scala/sygus/Literal.scala
+++ src/main/scala/sygus/Literal.scala
@@ -1,11 +1,17 @@
 package sygus
 
-sealed trait Literal
+trait Literal {
+  def value: Any
+}
+
 case class IntConst(value: Int) extends Literal { override def toString = value.toString }
 case class RealConst(value: Double) extends Literal { override def toString = value.toString }
 case class BoolConst(value: Boolean) extends Literal { override def toString = value.toString }
 case class BVConst(value: List[Boolean]) extends Literal { override def toString = value.toString }
 case class StringConst(value: String) extends Literal { override def toString = value.toString }
-case class EnumConst(sort: String, ctor: String) extends Literal
+
+case class EnumConst(sort: String, ctor: String) extends Literal {
+  val value = ctor
+}
 
 // End ///////////////////////////////////////////////////////////////
--- src/main/scala/sygus14/SetLogicTheory.java
+++ src/main/scala/sygus14/SetLogicTheory.java
@@ -1,6 +1,7 @@
 package sygus14;
 
-enum SetLogicTheory { LIA, NIA, LRA, NRA, BV, Reals, Arrays, SLIA, ALL, S, QF_S }
+enum SetLogicTheory { LIA, NIA, LRA, NRA, BV, Reals, Arrays, SLIA, ALL, S, QF_S,
+                      QF_LIA, QF_NIA, QF_LRA, QF_NRA}
 
 // End ///////////////////////////////////////////////////////////////
 
